e47a23dd208e9a744c6170efac5af2ed1805c553
fasilpaloli <palolifasil@gmail.com>
2022-02-03 01:31:23 +0530

    face mesh 2

---
+++ 04_face_mesh2.js
@@ -0,0 +1,39 @@
+import {loadGLTF, loadTexture} from "./libs/loader.js";
+const THREE = window.MINDAR.FACE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    const mindarThree = new window.MINDAR.FACE.MindARThree({
+      container: document.body,
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    scene.add(light);
+
+    const faceMesh = mindarThree.addFaceMesh();
+    const texture = await loadTexture('./assets/facemesh/face-mask-template/Face_Mask_Template.png');
+    faceMesh.material.map = texture;
+    faceMesh.material.transparent = true;
+    faceMesh.material.needsUpdate = true;
+    scene.add(faceMesh);
+
+    document.querySelector("#switch").addEventListener("click", () => {
+        mindarThree.switchCamera();
+        console.log('camera');
+    });
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+
+    document.querySelector("#switch").addEventListener("click", () => {
+        mindarThree.switchCamera();
+        console.log('2 camera');
+    });
+      renderer.render(scene, camera);
+    });
+
+    
+  }
+  start();
+});
